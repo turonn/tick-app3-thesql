b70d4fe56091393d18ee169480efe86a8eb44df8
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-02-07 13:03:18 -0500

    added javascript to update cart with quantitiy of tickets in cart.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -17,16 +17,20 @@ class CartController < ApplicationController
   
     def adjust_tickets
       id = params[:id].to_i
-      var = params[:var].to_i
+      ticket_count = params[:ticket_count].to_i
       session[:cart].delete(id)
-      var.times {session[:cart] << id}
+      ticket_count.times {session[:cart] << id}
       redirect_to cart_path
+      #respond_to do |format|
+      #  format.json { render :index }
+      #  format.html { render :index }
+      #end
     end
 
     private
   
     def load_cart
-      @cartitems = Game.find(session[:cart])
+      @cartitems = Game.find(session[:cart]).sort
       @cart = session[:cart]
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   resources :tickets, only: [:show, :index]
   resources :users, except: [:delete]
   resources :cart, except: [:delete]
-  post 'cart/:id/:var/adjust_tickets/', to: 'cart#adjust_tickets', as: 'adjust_tickets'
+  post 'cart/:id/:ticket_count/adjust_tickets/', to: 'cart#adjust_tickets', as: 'adjust_tickets'
 end
