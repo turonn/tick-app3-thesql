5d63dfbc03e4a1e1a564a513ac81c25db3d0be1d
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-05-05 17:40:16 -0400

    addded extension in application.js file.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import "../stylesheets/custom_bulma"
+import "../stylesheets/custom_bulma.scss"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
